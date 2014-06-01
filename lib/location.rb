fe2aae2bf1b49a5613755c950dd2722ecf889027
Kate Morrow <kmorrow@newrelic.com>
2014-06-01 10:22:44 -0700

    improve a code comment on zip code handling.

--- lib/location.rb
+++ lib/location.rb
@@ -4,7 +4,8 @@ class Location
   field :latitude, type: Float
   field :longitude, type: Float
   field :region, type: String
-  field :zip_code #not a problem in OR, but what about zips starting in 0?
+  field :zip_code, type: String
+  #^ On use of String here: not a problem in OR, but elsewhere zips begin in 0
   field :zip_nine, type: String
   field :is_location_approximate, type: Boolean
   field :business_name, type: String
