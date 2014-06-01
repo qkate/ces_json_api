79ddb4bf3bb954778e49baeb7838534873e669de
Chuck Lauer Vose <vosechu@gmail.com>
2014-05-31 22:35:23 -0700

    use json mime-type; remove non-json format

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'mongoid', '~> 3.1.6'
 
 group :production do
--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ Bundler.require(:default)
 
 Mongoid.load!("config/mongoid.yml")
 
+require 'sinatra/json'
+
 # auto-require anything in /lib
 Dir.glob("lib/*") { |file| require_relative file }
 
@@ -27,12 +29,7 @@ post '/submit' do
   Sample.create(business_type: 'Retail', location: {latitude: 123}, collection_bins: [{name: 'Readily Recyclable'}])
 end
 
-get '/api/samples' do
+get '/api/samples.?:format?' do
   @samples = Sample.all
-  erb :list
-end
-
-get '/api/samples/:id' do |id|
-  @sample = Sample.find(id)
-  erb :show
+  json @samples
 end
